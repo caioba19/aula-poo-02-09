a985fe3595083c8e4fa20b47d8ff860845c54508
Caio Vinicius Santos da Costa Leite <157418334+caioba19@users.noreply.github.com>
2026-09-02 09:14:13 -0300

    Add pessoa class with attributes and method

---
+++ pessoa.java
@@ -0,0 +1,13 @@
+public class pessoa {
+    public int codigo;
+    public String nome;
+    public int idade;
+    public String cpf;
+
+    public void getDados() {
+        System.out.println("Codigo: " + codigo
+                + " | Nome: " + nome
+                + " | Idade: " + idade
+                + " | Cpf: " + cpf);
+    }
+}
